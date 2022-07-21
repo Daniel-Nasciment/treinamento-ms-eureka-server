ad5b5eb34ba19f58a9994f8327c7dee258ddbc54
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-21 20:56:37 -0300

    Comentario sobre eureka

--- src/main/java/br/com/alura/microservice/eureka/EurekaServerApplication.java
+++ src/main/java/br/com/alura/microservice/eureka/EurekaServerApplication.java
@@ -10,6 +10,13 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		
+		// O QUE É O EUREKA ?
+		
+		// É UM SERVIDOR ONDE OS DEMAIS MICRO SERVIÇOS IRÃO SE REGISTRAR
+		
+		// ISSO É UM SERVICE DISCOVERY
+		
 	}
 
 }
